ef3ffbd3e9fa9b8724a9bbcba63d6e6ba852200c
Evelyn <44404447+evelynwelsh@users.noreply.github.com>
2022-02-07 19:42:35 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Evelyn Welsh UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript Evelyn Welsh
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
