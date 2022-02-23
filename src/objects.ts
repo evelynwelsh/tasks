c72eef56fa7b71ab39631abc06e5971d280899ac
Evelyn <ejw2900@gmail.com>
2022-02-23 16:33:59 -0500

    Did 3

--- src/objects.ts
+++ src/objects.ts
@@ -48,7 +48,14 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    if (question.type === "short_answer_question") {
+        return true;
+    } else {
+        if (question.options.includes(answer)) {
+            return true;
+        }
+        return false;
+    }
 }
 
 /**
