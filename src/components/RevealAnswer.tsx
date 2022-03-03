ed66281f61a993da1425570112393c144a1213bb
Evelyn <ejw2900@gmail.com>
2022-03-03 15:31:24 -0500

    RevealAnswer seems to be functioning

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,14 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [shown, setShown] = useState<boolean>(false);
+    function showAns(): void {
+        setShown(!shown);
+    }
+    return (
+        <div>
+            <Button onClick={showAns}>Reveal Answer</Button>
+            {shown && <div>42</div>}
+        </div>
+    );
 }
