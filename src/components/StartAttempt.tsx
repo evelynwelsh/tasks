515ef6236aa3f864a35ce5e920579076c119c4f9
Evelyn <ejw2900@gmail.com>
2022-03-04 22:54:30 -0500

    Done all by holidays

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -3,5 +3,30 @@ import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
     const [inProgress, changeProgress] = useState<boolean>(false);
-    return <div>Start Attempt</div>;
+    const [attempts, changeAttempts] = useState<number>(4);
+    return (
+        <div>
+            <Button
+                onClick={() => {
+                    changeAttempts(attempts - 1);
+                    changeProgress(true);
+                }}
+                disabled={attempts <= 0 || inProgress}
+            >
+                Start Quiz: {attempts} attempts left
+            </Button>
+            <Button
+                onClick={() => changeProgress(false)}
+                disabled={!inProgress}
+            >
+                Stop Quiz
+            </Button>
+            <Button
+                onClick={() => changeAttempts(attempts + 1)}
+                disabled={inProgress}
+            >
+                Mulligan
+            </Button>
+        </div>
+    );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,23 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [left, setLeft] = useState<number>(1);
+    const [right, setRight] = useState<number>(2);
+    return (
+        <div>
+            Two Dice
+            <div>
+                <Button onClick={() => setLeft(d6())}>Roll Left</Button>
+                <Button onClick={() => setRight(d6())}>Roll Right</Button>
+            </div>
+            <div>
+                Left: <span data-testid="left-die">{left} </span>
+                Right: <span data-testid="right-die">{right}</span>
+            </div>
+            <div>
+                {left === right && left !== 1 && "Win!"}
+                {left === right && left === 1 && "Lose!"}
+            </div>
+        </div>
+    );
 }
