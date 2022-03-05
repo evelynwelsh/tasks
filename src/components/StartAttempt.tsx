c09c55f606c00697fe543ca7ab912d7df6970e65
Evelyn <ejw2900@gmail.com>
2022-03-04 20:56:41 -0500

    Did first 2

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,6 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
+    const [inProgress, changeProgress] = useState<boolean>(false);
     return <div>Start Attempt</div>;
 }
