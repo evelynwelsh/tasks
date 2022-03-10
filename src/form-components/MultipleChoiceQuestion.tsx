a5214ae9927e30f5f4239ece2a737442cfdb1983
Evelyn <ejw2900@gmail.com>
2022-03-09 22:56:05 -0500

    Finished MCQ

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-
+import { Form } from "react-bootstrap";
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 export function MultipleChoiceQuestion({
     options,
     expectedAnswer
@@ -7,9 +10,27 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [ans, setAns] = useState<string>(options[0]);
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="ansChoices">
+                <Form.Label>What is the answer?</Form.Label>
+                <Form.Select
+                    value={ans}
+                    onChange={(event: ChangeEvent) =>
+                        setAns(event.target.value)
+                    }
+                >
+                    {options.map((opt: string) => (
+                        <option key={opt} value={opt}>
+                            {opt}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>{expectedAnswer === ans ? "✔️" : "❌"}</div>
         </div>
     );
 }
