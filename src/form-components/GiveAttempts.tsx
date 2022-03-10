614d5c27f1e2ba3fa25c3b70c834b79ec741bcef
Evelyn <ejw2900@gmail.com>
2022-03-09 20:33:55 -0500

    New test thing

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
-
+import { Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 export function GiveAttempts(): JSX.Element {
+    const [attempts, setAttempts] = useState<number>(2);
+    const [add, setAdd] = useState<number>(0);
     return (
         <div>
             <h3>Give Attempts</h3>
+            <Button></Button>
+            <Button></Button>
         </div>
     );
 }
