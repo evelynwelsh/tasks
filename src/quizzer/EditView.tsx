ba59e44ac7d691fe4bbead4e419df20666a24a0b
Evelyn <ejw2900@gmail.com>
2022-04-02 17:43:10 -0400

    Trying to add EditView

---
+++ src/quizzer/EditView.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+// import { Row, Col, Container, Button, Form } from "react-bootstrap";
+import { Quiz } from "../interfaces/quiz";
+import { Question } from "../interfaces/question";
+import { MultipleChoiceQuestion } from "../form-components/MultipleChoiceQuestion";
+import { CheckAnswer } from "../form-components/CheckAnswer";
--- src/quizzer/QuestionView.tsx
+++ src/quizzer/QuestionView.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 // import { Row, Col, Container, Button, Form } from "react-bootstrap";
 import { Quiz } from "../interfaces/quiz";
 import { Question } from "../interfaces/question";
